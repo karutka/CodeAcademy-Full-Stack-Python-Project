b9121f7a11beeac55fff83cc601229f9028fc92b
karutka <kaste.rutka@gmail.com>
2022-11-15 13:31:11 +0200

    fix code formatting

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap5
 from flask_moment import Moment
 from flask_fontawesome import FontAwesome
-from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    current_user,
+    logout_user,
+    login_user,
+    login_required,
+)
 from flask_bcrypt import Bcrypt
 import forms
 
@@ -29,25 +36,31 @@ fa = FontAwesome(app)
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 
 class User(db.Model, UserMixin):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column('Username', db.String(64), unique=True, index=True, nullable=False)
-    password = db.Column('Password', db.String(64), unique=True, index=True, nullable=False)
+    username = db.Column(
+        "Username", db.String(64), unique=True, index=True, nullable=False
+    )
+    password = db.Column(
+        "Password", db.String(64), unique=True, index=True, nullable=False
+    )
+
 
 class Note(db.Model):
     __tablename__ = "note"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column("Title", db.String)
     text = db.Column("Text", db.String)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     user = db.relationship("User", lazy=True)
-    
+
+
 class Category(db.Model):
     __tablename__ = "category"
     id = db.Column(db.Integer, primary_key=True)
@@ -55,54 +68,63 @@ class Category(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     user = db.relationship("User", lazy=True)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/homepage/", methods=["GET", "POST"])
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/login", methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     form = forms.LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('index'))
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("index"))
         else:
-            flash('Login failed. Check username and password', 'danger')
-    return render_template('login.html', title='Log in', form=form)
+            flash("Login failed. Check username and password", "danger")
+    return render_template("login.html", title="Log in", form=form)
+
 
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     db.create_all()
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     form = forms.RegistrationForm()
     if form.validate_on_submit():
-        encrypted_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        encrypted_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
         user = User(username=form.username.data, password=encrypted_password)
         db.session.add(user)
         db.session.commit()
-        flash('Successfully signed up! You csn log in.', 'success')
-        return redirect(url_for('index'))
-    return render_template('register.html', title='Register', form=form)
+        flash("Successfully signed up! You csn log in.", "success")
+        return redirect(url_for("index"))
+    return render_template("register.html", title="Register", form=form)
 
-@app.route("/notes",  methods=['GET', 'POST'])
+
+@app.route("/notes", methods=["GET", "POST"])
 @login_required
 def notes():
     db.create_all()
@@ -118,41 +140,54 @@ def notes():
         all_notes = []
     return render_template("notes.html", all_notes=all_notes)
 
+
 @app.route("/create_note/<int:category_id>", methods=["GET", "POST"])
 @login_required
 def create_note(category_id):
     db.create_all()
     form = forms.NoteForm()
     categories = get_categories()
-    form.category.choices = [(str(category.id), category.category) for category in get_categories()]
+    form.category.choices = [
+        (str(category.id), category.category) for category in get_categories()
+    ]
 
     if form.validate_on_submit():
-        create_note = Note(title=form.title.data, text=form.text.data,
-                                 category_id=form.category.data, user_id=current_user.id)
+        create_note = Note(
+            title=form.title.data,
+            text=form.text.data,
+            category_id=form.category.data,
+            user_id=current_user.id,
+        )
         db.session.add(create_note)
         db.session.commit()
-        flash(f"Note created", 'success')
-        return redirect(url_for('notes'))
+        flash(f"Note created", "success")
+        return redirect(url_for("notes"))
     else:
-        selected_category = [category.id for category in categories if category_id == category.id]
+        selected_category = [
+            category.id for category in categories if category_id == category.id
+        ]
         if selected_category:
             form.category.default = str(selected_category[0])
             form.process()
         return render_template("create_note.html", form=form, categories=categories)
 
-@app.route("/update_note/<int:id>", methods=['GET', 'POST'])
+
+@app.route("/update_note/<int:id>", methods=["GET", "POST"])
 @login_required
 def update_note(id):
     form = forms.NoteForm()
     note = Note.query.get(id)
-    form.category.choices = [(str(category.id), category.category) for category in get_categories()]
+    form.category.choices = [
+        (str(category.id), category.category) for category in get_categories()
+    ]
     if form.validate_on_submit():
         note.title = form.title.data
         note.text = form.text.data
         note.category_id = form.category.data
         db.session.commit()
-        return redirect(url_for('notes'))
+        return redirect(url_for("notes"))
     return render_template("modify_note.html", form=form, note=note)
+
 
 @app.route("/delete_note/<int:id>")
 @login_required
@@ -160,7 +195,8 @@ def delete_note(id):
     note = Note.query.get(id)
     db.session.delete(note)
     db.session.commit()
-    return redirect(url_for('notes'))
+    return redirect(url_for("notes"))
+
 
 @login_required
 def get_category_name(id):
@@ -171,13 +207,20 @@ def get_category_name(id):
     else:
         return None
 
+
 @app.route("/categories")
 @login_required
 def categories():
     db.create_all()
     all_categories = get_categories()
     all_notes = get_notes()
-    return render_template('categories.html', title='Categories', all_categories=all_categories, all_notes=all_notes)
+    return render_template(
+        "categories.html",
+        title="Categories",
+        all_categories=all_categories,
+        all_notes=all_notes,
+    )
+
 
 def get_categories():
     try:
@@ -186,6 +229,7 @@ def get_categories():
         all_categories = []
     return all_categories
 
+
 @login_required
 def get_notes():
     try:
@@ -193,6 +237,7 @@ def get_notes():
     except:
         all_notes = []
     return all_notes
+
 
 @app.route("/create_category", methods=["GET", "POST"])
 @login_required
@@ -203,9 +248,10 @@ def create_category():
         create_category = Category(category=form.category.data, user_id=current_user.id)
         db.session.add(create_category)
         db.session.commit()
-        flash(f"New category was added", 'success')
-        return redirect(url_for('categories'))
-    return render_template('create_category.html', title='New category', form=form)
+        flash(f"New category was added", "success")
+        return redirect(url_for("categories"))
+    return render_template("create_category.html", title="New category", form=form)
+
 
 @app.route("/delete_category/<int:id>")
 @login_required
@@ -213,17 +259,18 @@ def delete_category(id):
     note = Category.query.get(id)
     db.session.delete(note)
     db.session.commit()
-    
+
     notes = get_notes()
     combined_notes = [note.id for note in notes if id == note.category_id]
 
     if combined_notes:
         for combined_note in combined_notes:
             delete_note(combined_note)
-    
-    return redirect(url_for('categories'))
 
-@app.route("/modify_category/<int:id>", methods=['GET', 'POST'])
+    return redirect(url_for("categories"))
+
+
+@app.route("/modify_category/<int:id>", methods=["GET", "POST"])
 @login_required
 def modify_category(id):
     form = forms.CategoryForm()
@@ -231,23 +278,26 @@ def modify_category(id):
     if form.validate_on_submit():
         category.category = form.category.data
         db.session.commit()
-        return redirect(url_for('categories'))
+        return redirect(url_for("categories"))
     return render_template("modify_category.html", form=form, category=category)
 
-@app.route("/search", methods=['GET', 'POST'])
+
+@app.route("/search", methods=["GET", "POST"])
 @login_required
 def search():
     db.create_all()
     wanted_title = None
     wanted_category = []
     search_form = forms.SearchForm()
-    search_form.categories.choices = [(str(category.id), category.category) for category in get_categories()]
+    search_form.categories.choices = [
+        (str(category.id), category.category) for category in get_categories()
+    ]
     if search_form.validate_on_submit():
         wanted_title = search_form.title.data
         wanted_category = search_form.categories.data
-        
+
     is_search = wanted_title or wanted_category
-    
+
     if is_search:
         try:
             all_notes = Note.query.filter_by(user_id=current_user.id).all()
@@ -269,7 +319,10 @@ def search():
             all_notes = []
     else:
         all_notes = []
-    return render_template('search.html', title='Search', all_notes=all_notes, search_form=search_form)
+    return render_template(
+        "search.html", title="Search", all_notes=all_notes, search_form=search_form
+    )
+
 
 @app.errorhandler(404)
 def page_not_found(e):
--- forms.py
+++ forms.py
@@ -1,37 +1,53 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, BooleanField, StringField, PasswordField, FloatField, SelectField, SelectMultipleField, \
-    widgets
+from wtforms import (
+    SubmitField,
+    BooleanField,
+    StringField,
+    PasswordField,
+    FloatField,
+    SelectField,
+    SelectMultipleField,
+    widgets,
+)
 from wtforms.validators import DataRequired, ValidationError, EqualTo
 import app
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', [DataRequired()])
-    password = PasswordField('Password', [DataRequired()])
-    approved_password = PasswordField('Repeat password', [EqualTo('password', "Password must be identical")])
-    submit = SubmitField('Register')
+    username = StringField("Username", [DataRequired()])
+    password = PasswordField("Password", [DataRequired()])
+    approved_password = PasswordField(
+        "Repeat password", [EqualTo("password", "Password must be identical")]
+    )
+    submit = SubmitField("Register")
 
     def check_username(self, username):
         user = app.User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('This username is already taken. Please choose different one.')
+            raise ValidationError(
+                "This username is already taken. Please choose different one."
+            )
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', [DataRequired()])
-    password = PasswordField('Password', [DataRequired()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Log In')
-    
+    username = StringField("Username", [DataRequired()])
+    password = PasswordField("Password", [DataRequired()])
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Log In")
+
+
 class CategoryForm(FlaskForm):
-    category = StringField('Category', [DataRequired()])
-    submit = SubmitField('Add')
-    
+    category = StringField("Category", [DataRequired()])
+    submit = SubmitField("Add")
+
+
 class NoteForm(FlaskForm):
-    title = StringField('Note title', [DataRequired()])
-    text = StringField('Note text', [DataRequired()])
-    category = SelectField('Categories', choices=[])
-    submit = SubmitField('Submit')
-    
+    title = StringField("Note title", [DataRequired()])
+    text = StringField("Note text", [DataRequired()])
+    category = SelectField("Categories", choices=[])
+    submit = SubmitField("Submit")
+
+
 class MultiForm(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
