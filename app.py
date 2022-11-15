ac16bb7c2397c580ebb56c01f667c24cf9a2f030
karutka <kaste.rutka@gmail.com>
2022-11-15 12:44:08 +0200

    Handle 403, 404 and 500 errors with custom error pages

--- app.py
+++ app.py
@@ -264,3 +264,17 @@ def search():
     except:
         all_notes = []
     return render_template('search.html', title='Search', all_notes=all_notes, search_form=search_form)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template("403.html"), 403
